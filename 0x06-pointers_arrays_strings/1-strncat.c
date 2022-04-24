a8dec5d5a94c21e50d7da7af2daf6f54ba9f29bd
Heritage <alaoheritage700@gmail.com>
2022-04-24 11:54:47 +0100

    corrected

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,21 @@
 #include "main.h"
-/**
- *_strcat - concatenates two strings
- *@dest: A pointer to a character that will be changed
- *@src: A pointer to a character that will also be changed
- *Return: dest
- */
 
+/**
+ * _strcat - a function that concatenates two strings.
+ * @dest: an input string
+ * @src: an input string
+ * Return: A pointer to the resulting string
+ */
 char *_strcat(char *dest, char *src)
 {
-int i, j;
+	char *temp = dest;
 
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
+	while (*dest)
+		dest++;
 
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
+	while (*src)
+		*dest++ = *src++;
 
-return (dest);
+	*dest = '\0';
+	return (temp);
 }
