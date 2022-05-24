dbdf16219e9febbd3dd397ac9ad3e13e47c47173
Heritage <alaoheritage700@gmail.com>
2022-05-24 15:08:44 +0100

    add node 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,48 +1,28 @@
 #include "lists.h"
-
 /**
- * _strlen - find string length
- * @str: string
- * Return: length
- */
-int _strlen(const char *str)
-{
-	int len;
-
-	for (len = 0; str[len] != '\0'; len++)
-		;
-	return (len);
-}
-
-/**
- * add_node - add node to beginning of linked list
- * @head: linked list
- * @str: data for new node
- * Return: address of new element, or NULL if failed
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node; 
+	list_t *new;
+	size_t nchar;
 
-	if (str == NULL) 
-		return (NULL);
-	if (strdup(str) == NULL) 
-		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	return (NULL);
 
-	new_node = malloc(sizeof(list_t)); 
-	if (new_node == NULL)
-		return (NULL);
+	new->str = strdup(str);
 
-	new_node->str = strdup(str); 
-	new_node->len = _strlen(str);
+	for (nchar = 0; str[nchar]; nchar++);
 
-	if (head == NULL) 
-		new_node->next = NULL;
-	else
-		new_node->next = *head;
+	new->len = nchar;
+	new->next = *head;
+	*head = new;
 
-	*head = new_node; 
-
-	return (new_node);
+	return (*head);
 }
