027addc96b21d4e7643ad7be2859454db082bf47
Heritage <alaoheritage700@gmail.com>
2022-05-24 16:21:57 +0100

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,52 +1,41 @@
 #include "lists.h"
 
 /**
- * _strlen - find string length
- * @str: string
- * Return: length
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
-int _strlen(const char *str)
-{
-	int len;
 
-	for (len = 0; str[len] != '\0'; len++)
-		;
-	return (len);
-}
-
-/**
- * add_node_end - add node to end of linked list
- * @head: linked list
- * @str: data for new node
- * Return: address of new element, or NULL if failed
- */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *tmp; /* create new node */
+	list_t *new, *temp;
+	size_t nchar;
 
-	if (str == NULL) /* validate input */
-		return (NULL);
-	if (strdup(str) == NULL) /* check if malloc errored */
-		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	return (NULL);
 
-	new_node = malloc(sizeof(list_t)); /* malloc for new node */
-	if (new_node == NULL)
-		return (NULL);
+	new->str = strdup(str);
 
-	new_node->str = strdup(str); /* set node values */
-	new_node->len = _strlen(str);
-	new_node->next = NULL;
+	for (nchar = 0; str[nchar]; nchar++)
+	;
 
-	if (*head == NULL) /* if no list nodes, set new_node to beginning */
-		*head = new_node;
+	new->len = nchar;
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new;
+	}
 	else
 	{
-		tmp = *head;
-
-		while (tmp->next != NULL)
-			tmp = tmp->next;
-		tmp->next = new_node;
+		while (temp->next != NULL)
+		temp = temp->next;
+		temp->next = new;
 	}
 
-	return (new_node);
+	return (*head);
 }
