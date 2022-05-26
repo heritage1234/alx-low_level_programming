6e5841c3bf2440b770391221d0314947207acafc
Heritage <alaoheritage700@gmail.com>
2022-05-26 15:26:11 +0100

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number in a linked lists 
+ * listint_len - returns the number in a linked lists
  * @h: list
- * Return of nodes in list
+ * Return: of nodes in list
  */
 
-size_t listint_len(const listint_len *h) 
+size_t listint_len(const listint_len *h)
 {
-  	size_t num_nodes = 0;
+	size_t num_nodes = 0;
 
 	while (h != NULL)
 	{
 		num_nodes += 1;
 		h = h->next;
-  }
+}
 	return (num_nodes);
 }
